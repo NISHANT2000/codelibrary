d3cc212cb9075fcb6292f6f94e3a9d03deb1bca3
NISHANT_RS <47104971+NISHANT2000@users.noreply.github.com>
2021-06-10 11:41:12 +0530

    Create countSetbits(logn(1--n)).cpp

---
+++ countSetbits(logn(1--n)).cpp
@@ -0,0 +1,14 @@
+int findLargestPower(int n)
+{
+    int x = 0;
+    while ((1 << x) <= n)
+        x++;
+    return x - 1;
+}
+ int countSetBits(int n)
+    {
+        if (n <= 1)
+        return n;
+    int x = findLargestPower(n);
+    return (x * pow(2, (x - 1))) + (n - pow(2, x) + 1) + countSetBits(n - pow(2, x));
+    }
